e3669dbca42d7471e2fee09dd536058761cd862b
Natalia Timote <nataliamirian.nt@gmail.com>
2025-02-01 23:16:36 -0300

    implementando rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutMe from "./pages/About-me";
 import Home from "./pages/Home";
 
-const page = window.location.pathname === '/' ? <Home /> : <AboutMe />;
-
 function App() {
-  return page;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="*" element={<div>Página não encontrada!</div>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
