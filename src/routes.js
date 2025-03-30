36dbeb1a904fb03063602c9678fa5bdf92636c99
Natalia Timote <nataliamirian.nt@gmail.com>
2025-03-30 20:43:45 -0300

    inicio

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutMe from "./pages/About-me";
 import Home from "pages/Home";
 import Menu from "./components/Menu";
+import Footer from "components/Footer";
 
 function AppRoutes() {
   return (
@@ -14,6 +15,8 @@ function AppRoutes() {
         <Route path="/about-me" element={<AboutMe />} />
         <Route path="*" element={<div>Página não encontrada!</div>} />
       </Routes>
+
+      <Footer />
     </BrowserRouter>
   )
 }
